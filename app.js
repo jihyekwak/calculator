fe6ae43281d6ba66dd3f0cd51a39664e519821df
jihyekwak <jihyekwak53@gmail.com>
2023-02-16 22:35:41 -0800

    Add clear btn eventlistener

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const resultValue = document.querySelector("#result");
 const numberBtns = Array.from(document.querySelectorAll(".number"));
 const operatorBtns = Array.from(document.querySelectorAll(".operator"));
+const clearBtn = document.querySelector("#clear");
 
 let operator = null;
 
@@ -16,5 +17,11 @@ function onOperatorBtnClick(event) {
     return operator;
 }
 
+function onClearBtnClick() {
+    resultValue.innerHTML = 0;
+    operator = null;
+}
+
 numberBtns.forEach(number => number.addEventListener("click", onNumberBtnClick));
 operatorBtns.forEach(operator => operator.addEventListener("click", onOperatorBtnClick));
+clearBtn.addEventListener("click", onClearBtnClick);
